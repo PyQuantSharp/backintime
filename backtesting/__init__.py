040fddc2fa3ff5f1104843bc7dafeca8958083b2
Akim Mukhtarov <akimismyname@gmail.com>
2022-02-18 17:16:32 +0300

    add import for candles providers

--- backtesting/__init__.py
+++ backtesting/__init__.py
@@ -1,5 +1,9 @@
 from .backtester import Backtester
 from .trading_strategy import TradingStrategy
-from .market_data_analyzer import MarketDataAnalyzer
-from .timeframes import Timeframes
 from .candle_properties import CandleProperties
+from .timeframes import Timeframes
+from .candles_providers import (
+    BinanceApiCandles,
+    TimeframeDump,
+    TimeframeDumpScheme
+)
--- backtesting/candles_providers/__init__.py
+++ backtesting/candles_providers/__init__.py
@@ -1,1 +1,2 @@
-from .candles_provider import CandlesProvider
+from .binance_api_candles import BinanceApiCandles
+from .timeframe_dump import TimeframeDump, TimeframeDumpScheme
