0d932c219e84dd03f81594ac43257bd8af429e46
Akim Mukhtarov <akimismyname@gmail.com>
2022-02-18 19:54:05 +0300

    add helping function to read input OHLCV to Candle buffer

--- backtesting/candles_providers/timeframe_dump/timeframe_dump.py
+++ backtesting/candles_providers/timeframe_dump/timeframe_dump.py
@@ -1,4 +1,5 @@
 from .timeframe_dump_scheme import TimeframeDumpScheme
+from .utils import to_candle
 from ..candles_provider import CandlesProvider
 from ...timeframes import Timeframes
 
@@ -13,30 +14,26 @@ class TimeframeDump(CandlesProvider):
             filename: str,
             timeframe_tag: Timeframes,
             scheme: TimeframeDumpScheme = TimeframeDumpScheme()
-            ):
+    ):
         # scheme specifies indexes to use for fetching candle' open time and OHLC info
         self._scheme = scheme
         self._data = pd.read_csv(
-            filename, sep=';', parse_dates=[scheme.open_time_idx, scheme.close_time_idx]
+            filename,
+            sep=';',
+            parse_dates=[scheme.open_time_idx, scheme.close_time_idx]
         )
         self._gen = None
-        self._prev_time = None
         super().__init__(timeframe_tag)
 
     def current_date(self):
-        #
         if not self._start_date:
             return None
-
         ticks = self.get_ticks()
         time_passed = datetime.timedelta(
-            seconds=ticks*self.candle_duration()
-        )
+            seconds=ticks*self.candle_duration())
         return self._start_date + time_passed
 
-
     def next(self) -> None:
-
         if not self._gen:
             self._gen = iter(self._data.iterrows())
 
@@ -49,6 +46,7 @@ class TimeframeDump(CandlesProvider):
             while open_time < self._start_date:
                 _, row = next(self._gen)
                 open_time = row[self._scheme.open_time_idx]
+        '''
         # snippet to catch malformed
         # input with time loops
         # for debug mostly
@@ -57,14 +55,9 @@ class TimeframeDump(CandlesProvider):
                 # skip rows with invalid date
                 _, row = next(self._gen)
                 open_time = row[self._scheme.open_time_idx]
-
         self._prev_time = open_time
         # end snippet
+        '''
         self._candle_buffer.open_time = open_time
-        self._candle_buffer.close_time = row[self._scheme.close_time_idx]
-        self._candle_buffer.open = row[self._scheme.open_idx]
-        self._candle_buffer.high = row[self._scheme.high_idx]
-        self._candle_buffer.low = row[self._scheme.low_idx]
-        self._candle_buffer.close = row[self._scheme.close_idx]
-        self._candle_buffer.volume = row[self._scheme.volume_idx]
+        to_candle(row, scheme, self._candle_buffer)
         self._tick_counter.increment()
