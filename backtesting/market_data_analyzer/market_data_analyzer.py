0736374344761bcd04ef4a62dedb5b65f2e68aac
Akim Mukhtarov <akimismyname@gmail.com>
2022-02-18 15:49:01 +0300

    now use lambda instead of builder class as less verbose solution

--- backtesting/market_data_analyzer/market_data_analyzer.py
+++ backtesting/market_data_analyzer/market_data_analyzer.py
@@ -1,6 +1,5 @@
 from typing import Iterable
 
-from ..oscillators.oscillator_builder import OscillatorBuilder
 from ..candles_providers import CandlesProvider
 from ..market_data_storage import MarketDataStorage
 
@@ -10,21 +9,15 @@ class MarketDataAnalyzer:
     def __init__(
             self,
             market_data: CandlesProvider,
-            oscillators: Iterable[OscillatorBuilder]
-            ):
-
-        if not len(oscillators):
-            raise ValueError('Oscillators list must not be empty')
-
+            oscillators: Iterable[Callable]
+    ):
         self._values = MarketDataStorage(market_data)
-        oscillators = map(lambda x: x.build(), oscillators)
+        # init oscillators
+        oscillators = map(lambda x: x(self._values), oscillators)
         # Маппим осцилляторы к их имени для random access
         self._oscillators = {
             oscillator.get_name() : oscillator
-                for oscillator in (
-                    oscillator.set_market_data(self._values)
-                        for oscillator in oscillators
-                )
+                for oscillator in oscillators
         }
 
     def update(self) -> None:
